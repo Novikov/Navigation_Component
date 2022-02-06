a9241ecb059262f33bd2cc12d91c212a9a3937ff
Igor Novikov <igor.novikov@umbrellait.com>
2022-02-06 18:23:20 +0300

    savepoint

--- simple_navigation/src/main/java/com/example/navcomponent/MainActivity.kt
+++ simple_navigation/src/main/java/com/example/navcomponent/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.navcomponent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var controller: NavController
+
+    @SuppressLint("RestrictedApi")
+    private val listener = NavController.OnDestinationChangedListener { controller, destination, arguments ->
+        Log.i("ADSSADDSA", "${controller.backStack.size} ")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        controller = navHostFragment.navController
+    }
+
+    override fun onResume() {
+        super.onResume()
+        controller.addOnDestinationChangedListener(listener)
+    }
+
+    override fun onPause() {
+        controller.removeOnDestinationChangedListener(listener)
+        super.onPause()
     }
 }
